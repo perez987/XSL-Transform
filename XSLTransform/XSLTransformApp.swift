06ccebd7ddb9eb48588a173407ae8337e5c5ca5d
Emilio P Egido <eaperez@me.com>
2026-02-03 18:01:04 +0100

    Add language selector and locales

--- XSLTransform/XSLTransformApp.swift
+++ XSLTransform/XSLTransformApp.swift
@@ -2,11 +2,27 @@ import SwiftUI
 
 @main
 struct XSLTransformApp: App {
+    
+    @State private var isLanguageSelectorPresented = false
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .sheet(isPresented: $isLanguageSelectorPresented) {
+                    LanguageSelectorView()
+                }
         }
-        .windowStyle(.hiddenTitleBar)
-        .windowResizability(.contentSize)
-    }
+                .windowStyle(.hiddenTitleBar)
+                .windowResizability(.contentSize)
+            
+            // Language menu
+                .commands {
+                    CommandMenu(NSLocalizedString("menu_language", comment: "Language menu")) {
+                        Button(NSLocalizedString("menu_select_language", comment: "Select Language menu item")) {
+                            isLanguageSelectorPresented = true
+                        }
+                        .keyboardShortcut("l", modifiers: .command)
+                    }
+                }
+        }
 }
