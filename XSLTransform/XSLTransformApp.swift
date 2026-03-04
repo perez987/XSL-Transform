e81763c13d56dd5860f992d0157b79d2d67843ca
Emilio P Egido <eaperez@me.com>
2026-03-04 20:58:08 +0100

    Add Sparkle updater system

--- XSLTransform/XSLTransformApp.swift
+++ XSLTransform/XSLTransformApp.swift
@@ -1,9 +1,11 @@
 import SwiftUI
+import Sparkle
 
 @main
 struct XSLTransformApp: App {
     
     @State private var isLanguageSelectorPresented = false
+    @StateObject private var updaterController = UpdaterController()
 
     var body: some Scene {
         WindowGroup {
@@ -15,8 +17,24 @@ struct XSLTransformApp: App {
                 .windowStyle(.hiddenTitleBar)
                 .windowResizability(.contentSize)
             
-            // Language menu
                 .commands {
+                    // Updater menu
+                    CommandGroup(after: .appInfo) {
+                        Button(
+                            NSLocalizedString(
+                                "Check for Updates...",
+                                comment: "Menu item to check for app updates"
+                            ),
+        //                    systemImage: "square.and.arrow.down.badge.checkmark"
+                            systemImage: "arrow.triangle.2.circlepath"
+                        ) {
+                            updaterController.checkForUpdates()
+                        }
+                        .keyboardShortcut("u", modifiers: [.command])
+                        .disabled(!updaterController.canCheckForUpdates)
+                    }
+                    // Language menu
+                    CommandGroup(replacing: .newItem) { }
                     CommandMenu(NSLocalizedString("menu_language", comment: "Language menu")) {
                         Button(NSLocalizedString("menu_select_language", comment: "Select Language menu item")) {
                             isLanguageSelectorPresented = true
