d9748ddce2f0227cb763715b4047508d293924a9
Emilio P Egido <eaperez@me.com>
2026-08-10 11:45:32 +0200

    Remove Sparkle updater integration

--- XSLTransform/XSLTransformApp.swift
+++ XSLTransform/XSLTransformApp.swift
@@ -1,11 +1,9 @@
 import SwiftUI
-import Sparkle
 
 @main
 struct XSLTransformApp: App {
     
     @State private var isLanguageSelectorPresented = false
-    @StateObject private var updaterController = UpdaterController()
 
     var body: some Scene {
         WindowGroup {
@@ -18,21 +16,6 @@ struct XSLTransformApp: App {
                 .windowResizability(.contentSize)
             
                 .commands {
-                    // Updater menu
-                    CommandGroup(after: .appInfo) {
-                        Button(
-                            NSLocalizedString(
-                                "Check for Updates...",
-                                comment: "Menu item to check for app updates"
-                            ),
-        //                    systemImage: "square.and.arrow.down.badge.checkmark"
-                            systemImage: "arrow.triangle.2.circlepath"
-                        ) {
-                            updaterController.checkForUpdates()
-                        }
-                        .keyboardShortcut("u", modifiers: [.command])
-                        .disabled(!updaterController.canCheckForUpdates)
-                    }
                     // Language menu
                     CommandGroup(replacing: .newItem) { }
                     CommandMenu(NSLocalizedString("menu_language", comment: "Language menu")) {
